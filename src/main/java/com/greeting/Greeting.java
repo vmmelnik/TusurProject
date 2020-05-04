1a496529b9ba66565e0cc49b04295e6df511acc2
Vladimir Melnikov <melnikov55vm@gmail.com>
2020-05-04 19:28:04 +0300

    Creating class Greeting

---
+++ src/main/java/com/greeting/Greeting.java
@@ -0,0 +1,9 @@
+package com.greeting;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        System.out.println("Привет слушателям курса \"Профессиональная разработка на JAVA\"!!!");
+    }
+
+}
