fae5bc820108456169b0a014fb26fb84783e1247
vmmelnik <melnikov55vm@gmail.com>
2021-01-09 22:35:32 +0300

    Update project

---
+++ src/main/java/com/greeting/TusurProject.java
@@ -0,0 +1,9 @@
+package com.greeting;
+
+public class TusurProject {
+
+    public static void main(String[] args) {
+        System.out.println("Привет слушателям курса \"Профессиональная разработка на JAVA\"!!!");
+    }
+
+}
